a6a75d7865bcb26b6e3806be7b90a6ee29253ae6
valmassoi <rvalmassoi@gmail.com>
2016-08-07 15:46:22 -0700

    add server w/ temp routes

--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 
 const express = require('express')
 const http = require('http')
-const routes = require('./app/routes/someroute')
-const mongo = require('mongodb').MongoClient
+const router = require('./app/router')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
 
 const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27018/data'
+mongoose.connect(dbUrl)
 
-app.use(express.static(__dirname+'/public/'))
+app.use(express.static(__dirname))
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.enable('trust proxy')
+router(app)
 
 app.get('*', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
